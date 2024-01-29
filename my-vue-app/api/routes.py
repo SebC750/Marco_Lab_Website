5158152f6a9acc6a60203db677179b081cb8f4f3
SebC750 <SebC89999@gmail.com>
2024-01-29 10:18:15 -0500

    initialize project

---
+++ my-vue-app/api/routes.py
@@ -0,0 +1,4 @@
+import flask
+
+def authenticate():
+    return 0
---
+++ my-vue-app/src/pages/main.jsx
@@ -0,0 +1,14 @@
+function MainPage() {
+    
+  
+    return (
+      <>
+        <div>
+            <h1> Welcome to Marco's Lab </h1>
+          </div>
+      </>
+    )
+  }
+  
+  export default MainPage
+  
