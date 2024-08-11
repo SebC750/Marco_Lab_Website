6eb9532f69d590bee11bc6551b240aa5a2fa26d1
SebC750 <SebC89999@gmail.com>
2024-08-10 22:52:13 -0400

    git commit

--- my-vue-app/src/pages/About.jsx
+++ my-vue-app/src/pages/About.jsx
@@ -1,9 +1,13 @@
 import Navbar from "./Navbar.jsx" 
+import "./stylesheets/About.css"
 function About(){
     return (
         <div> 
             <Navbar> </Navbar>
-            <h1> About Us! </h1>
+            <div className="about-title">
+               <h1> About Us! </h1>
+            </div>
+            <h1> FAQ </h1>
         </div>
     )
 }
