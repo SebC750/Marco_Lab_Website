17dc9c82354a9c5cf5d33a9dc5a1e189c049d04a
SebC750 <SebC89999@gmail.com>
2024-11-01 15:11:05 -0400

    Populating footer and added field of study section

---
+++ my-vue-app/src/pages/components/FieldOfStudySection.jsx
@@ -0,0 +1,45 @@
+import fieldsOfStudy from "../datasets/fieldsofstudy.json"
+import "../stylesheets/Home.css"
+
+const FieldOfStudySection = () => {
+    return (
+        <div className="card">
+            <div className="card-body">
+                <div className="card-title d-flex justify-content-center" id="title-border">
+                    <h1> Fields of study </h1>
+                </div>
+                <div className="container">
+                    <div className="row row-cols-lg-3 row-cols-md-2 row-cols-sm-1 g-4">
+                        {fieldsOfStudy && (
+                            <>
+                                {fieldsOfStudy.map((val, index) => (
+                                    <div className="col" key={index}>
+                                        <div className="card h-100 text-white bg-info d-flex flex-column" id="card-container">
+                                            <img src={val.card_picture} className="card-img-top" id="field-cards" alt="Field of Study"/>
+                                            <div className="card-body d-flex flex-column p-5">
+                                                <div className="card-title">
+                                                    <h2> {val.field} </h2>
+                                                </div>
+                                                <div className="card-text mb-3">
+                                                    {val.brief_description}
+                                                </div>
+                                                <div className="mt-auto">
+                                                    <button className="btn btn-light d-block w-100 btn-lg rounded-0">
+                                                        <i className="bi bi-file-earmark-text"></i>
+                                                        <a href={val.article_link} className="text-dark"> Article link</a>
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))}
+                            </>
+                        )}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FieldOfStudySection
--- my-vue-app/src/pages/components/OurTeamSection.jsx
+++ my-vue-app/src/pages/components/OurTeamSection.jsx
@@ -4,18 +4,18 @@ const OurTeamSection = () => {
     <>
       <div className="card bg-info text-white">
         <div className="row">
-          <div className="col">
+          <div className="col-12 col-md-6 order-last order-md-first">
             <img src="marco-lab-pictures/marco_lab_team.jpg" class="img-fluid w-100 h-100" alt="the research team" />
           </div>
-          <div className="col">
+          <div className="col-12 col-md-6">
             <div className="card-body p-5">
-              <h1 className="card-title"> 
+              <h1 className="card-title" id="title-border"> 
                  Our Team
               </h1>
               <div className="card-text">
                 Our research  
               </div>
-              <button className="btn btn-info btn-lg"></button>
+              <button className="btn btn-light rounded-0 btn-lg d-flex justify-content-end"> Meet the rest of the team!</button>
             </div>
           </div>
         </div>
